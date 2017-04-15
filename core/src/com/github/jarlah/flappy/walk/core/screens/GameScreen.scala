f58aed9ee58f47340b8ba89a8761970b89b6e3bb
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-04-15 17:30:54 +0200

    making vals private ++

--- core/src/com/github/jarlah/flappy/walk/core/screens/GameScreen.scala
+++ core/src/com/github/jarlah/flappy/walk/core/screens/GameScreen.scala
@@ -10,39 +10,40 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
 
 case class GameScreen(game: FlappyWalk) extends Screen {
 
-  val camera = new OrthographicCamera(game.width, game.height)
-
+  private val camera = new OrthographicCamera(game.width, game.height)
   camera.setToOrtho(false, game.width, game.height)
   camera.update()
 
-  val initialCameraYPos: Float = camera.position.y
-  val initialCameraXPos: Float = camera.position.x
+  private val initialCameraYPos = camera.position.y
+  private val initialCameraXPos = camera.position.x
 
-  val tiledMap: TiledMap = new TmxMapLoader().load("levels/gameart2d-desert.tmx")
-  val tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap)
-  val tileLayer: TiledMapTileLayer = tiledMap.getLayers.get("foreground").asInstanceOf[TiledMapTileLayer]
-  val tileHeight: Float = tileLayer.getHeight * tileLayer.getTileHeight
-  val tileWidth: Float = tileLayer.getWidth * tileLayer.getTileWidth
+  private val tiledMap = new TmxMapLoader().load("levels/gameart2d-desert.tmx")
+  private val tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap)
+  private val tileLayer = tiledMap.getLayers.get("foreground").asInstanceOf[TiledMapTileLayer]
+  private val tileHeight = tileLayer.getHeight * tileLayer.getTileHeight
+  private val tileWidth = tileLayer.getWidth * tileLayer.getTileWidth
+
+  private val speed = 4f
 
   def handleInput(): Unit = {
     if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-      if ((camera.position.x + 10) < (initialCameraXPos + tileWidth)) {
-        camera.translate(+10, 0)
+      if ((camera.position.x + speed) < (initialCameraXPos + tileWidth - game.width)) {
+        camera.translate(+speed, 0)
       }
     }
     if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-      if ((camera.position.x - 10) > initialCameraXPos) {
-        camera.translate(-10, 0)
+      if ((camera.position.x - speed) > initialCameraXPos) {
+        camera.translate(-speed, 0)
       }
     }
     if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-      if ((camera.position.y + 10) < (initialCameraYPos + tileHeight)) {
-        camera.translate(0, +10)
+      if ((camera.position.y + speed) < (initialCameraYPos + tileHeight - game.height)) {
+        camera.translate(0, +speed)
       }
     }
     if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-      if ((camera.position.y - 10) > initialCameraYPos) {
-        camera.translate(0, -10)
+      if ((camera.position.y - speed) > initialCameraYPos) {
+        camera.translate(0, -speed)
       }
     }
   }
